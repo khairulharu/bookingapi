c5113f70735cd84e41ee0f8568280e93c9e4181b
khairulharu <khairulaswad719@gmail.com>
2023-11-02 17:55:20 +0800

    testing

--- internal/config/loader.go
+++ internal/config/loader.go
@@ -1,20 +1,27 @@
 package config
 
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
 func Get() *Config {
-	// if err := godotenv.Load(".env"); err != nil {
-	// 	log.Fatalf("error when load env: %v", err.Error())
-	// }
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("error when load env: %v", err.Error())
+	}
 	return &Config{
 		Database{
-			Host: "ep-cold-pine-25979554.ap-southeast-1.aws.neon.fl0.io",
-			Port: "5432",
-			User: "fl0user",
-			Pass: "4WDZnMg0TEsq",
-			Name: "booking",
+			Host: os.Getenv("DB_HOST"),
+			Port: os.Getenv("DB_PORT"),
+			User: os.Getenv("DB_USER"),
+			Pass: os.Getenv("DB_PASS"),
+			Name: os.Getenv("DB_NAME"),
 		},
 		Server{
-			Host: "",
-			Port: "8080",
+			Host: os.Getenv("SRV_PORT"),
+			Port: os.Getenv("PORT"),
 		},
 	}
 }
