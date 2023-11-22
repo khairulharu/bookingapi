d32b65862855386fabbaac8b07145910879b5094
khairulharu <khairulaswad719@gmail.com>
2023-11-22 17:36:41 +0800

    local env

--- internal/config/loader.go
+++ internal/config/loader.go
@@ -1,14 +1,14 @@
 package config
 
 import (
+	"log"
 	"os"
 
 	"github.com/joho/godotenv"
 )
 
 func Get() *Config {
-
-	if err := godotenv.Load(); err != nil {
+	if os.Getenv("APP_ENV") == "prod" {
 		return &Config{
 			Database{
 				Host: os.Getenv("PROD_DB_HOST"),
@@ -23,20 +23,26 @@ func Get() *Config {
 				Port: os.Getenv("PORT"),
 			},
 		}
-	}
-	return &Config{
-		Database{
-			Host: os.Getenv("DB_HOST"),
-			Port: os.Getenv("DB_PORT"),
-			User: os.Getenv("DB_USER"),
-			Pass: os.Getenv("DB_PASS"),
-			Name: os.Getenv("DB_NAME"),
-			SSL:  os.Getenv("DB_SSL"),
-		},
-		Server{
-			Host: os.Getenv("SRV_HOST"),
-			Port: os.Getenv("PORT"),
-		},
+	} else {
+
+		if err := godotenv.Load(); err != nil {
+			log.Printf("cannot load env : %s", err.Error())
+		}
+		return &Config{
+			Database{
+				Host: os.Getenv("DB_HOST"),
+				Port: os.Getenv("DB_PORT"),
+				User: os.Getenv("DB_USER"),
+				Pass: os.Getenv("DB_PASS"),
+				Name: os.Getenv("DB_NAME"),
+				SSL:  os.Getenv("DB_SSL"),
+			},
+			Server{
+				Host: os.Getenv("SRV_HOST"),
+				Port: os.Getenv("PORT"),
+			},
+		}
+
 	}
 
 }
